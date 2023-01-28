ad238a1ccf960f151da7fc877dd7fad0e1f36fda
lacos <lacoslaiden1@yandex.ru>
2023-01-28 18:19:29 +0300

    обновлен еще один тест

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-     return 'Hello, World!'
-
-if __name__ == "__main__":
-     app.run(debug=True, host='0.0.0.0')
 from flask import Flask, request
 from getpass import getpass
 from mysql.connector import connect, Error
